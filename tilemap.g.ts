f67ad54aed7dfa006d2680a2f98228fbe01f9386
Andrew Ekenes <84550532+andrewekenes@users.noreply.github.com>
2021-05-21 15:53:21 -0700

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -5,8 +5,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
@@ -28,6 +26,8 @@ namespace myTiles {
     export const tile12 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -78,8 +78,6 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "myTile":
             case "tile1":return tile1;
-            case "myTile1":
-            case "tile3":return tile3;
             case "myTile0":
             case "tile2":return tile2;
             case "myTile2":
@@ -102,6 +100,8 @@ namespace myTiles {
             case "tile12":return tile12;
             case "myTile11":
             case "tile13":return tile13;
+            case "myTile1":
+            case "tile3":return tile3;
         }
         return null;
     })
